8c9be613da802a8d96373383329a122b9bac16f4
silviu <silviu@openline.ai>
2023-09-26 12:25:51 +0300

    fetch test files

--- graphql-coverage/main.go
+++ graphql-coverage/main.go
@@ -4,15 +4,73 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"regexp"
 	"strings"
 )
 
 func main() {
-	repoURL := "https://api.github.com/repos/openline-ai/openline-customer-os/contents/packages/server/customer-os-api/graph/schemas"
+	baseUrl := "https://api.github.com/repos/openline-ai/openline-customer-os/contents/packages/server/customer-os-api/graph/"
+	getQueriesMutations(baseUrl)
+	getTestsForQueriesMutations(baseUrl)
+	//computeCoverage()
+}
 
-	resp, err := http.Get(repoURL)
+func getTestsForQueriesMutations(baseUrl string) {
+	// Define the GitHub API URL for the repository's contents
+	resolversIntegrationTestsSource := baseUrl + "resolver"
+
+	// Send a GET request to the GitHub API
+	resp, err := http.Get(resolversIntegrationTestsSource)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Check the response status code
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("HTTP Status:", resp.Status)
+		return
+	}
+
+	// Read and parse the response JSON
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	// Unmarshal the JSON into a slice of GitHubContent structs
+	var contents []GitHubContent
+	if err := json.Unmarshal(body, &contents); err != nil {
+		fmt.Println("Error parsing JSON:", err)
+		return
+	}
+
+	// Initialize a slice to store file names
+	var fileNames []string
+
+	// Iterate through the contents and filter files ending with ".resolvers_it_test.go"
+	for _, content := range contents {
+		if isFile(content) && strings.HasSuffix(content.Name, ".resolvers_it_test.go") {
+			//fmt.Println(content.Name)
+			fileNames = append(fileNames, content.Name)
+		}
+	}
+
+	// Print the list of file names
+	fmt.Println("File Names:")
+	for _, fileName := range fileNames {
+		fmt.Println(fileName)
+	}
+}
+
+func getQueriesMutations(baseUrl string) {
+	schemasSource := baseUrl + "schemas"
+
+	resp, err := http.Get(schemasSource)
 	if err != nil {
 		fmt.Println("Error making the request:", err)
 		return
@@ -83,8 +141,18 @@ func funcName(queriesMutations *regexp.Regexp, schemaContent []byte) []string {
 				queriesMutationName := match[1]
 				queriesMutationsNames = append(queriesMutationsNames, queriesMutationName)
 			}
-
 		}
 	}
 	return queriesMutationsNames
+}
+
+func isFile(content GitHubContent) bool {
+	return content.Type == "file" || content.Type == false
+}
+
+type GitHubContent struct {
+	Name    string      `json:"name"`
+	Path    string      `json:"path"`
+	Type    interface{} `json:"type"`
+	HTMLURL string      `json:"html_url"`
 }
